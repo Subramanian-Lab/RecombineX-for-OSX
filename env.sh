56c61cbdf4d7b4fc44bc6a00f07319da275e3608
kaldiraz <150075889+kaldiraz@users.noreply.github.com>
2024-10-15 21:59:19 +0530

    Installed SRAtoolkit in build; added test_project directory into .gitignore

--- env.sh
+++ env.sh
@@ -2,3 +2,4 @@
 
 export RECOMBINEX_HOME="/Users/kaldiraz/Documents/recombinex_for_mac"
 export BUILD_DIR="/Users/kaldiraz/Documents/recombinex_for_mac/build"
+export SRA_DIR="/Users/kaldiraz/Documents/recombinex_for_mac/build/sratoolkit.3.1.1-mac-x86_64/bin"
